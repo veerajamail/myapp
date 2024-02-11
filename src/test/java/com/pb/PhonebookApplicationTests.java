dbc33248724335bd98108bf744e641d07416f36a
veerajamail <veerajamail@gmail.com>
2024-02-11 06:46:39 +0000

    test case corrected

--- src/test/java/com/pb/PhonebookApplicationTests.java
+++ src/test/java/com/pb/PhonebookApplicationTests.java
@@ -1,13 +1,16 @@
 package com.pb;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class PhonebookApplicationTests {
 
 	@Test
-	void contextLoads() {
+      void contextLoads() {
 	}
 
 }
